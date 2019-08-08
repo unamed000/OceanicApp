6f30a0baa163e53b30d12e1b6fe027766811b015
Huy Vu <Huy Vu@DESKTOP-PVLLDB0>
2019-08-08 10:28:19 +0700

    ! update test data

---
+++ ReactDemo/Db/Scripts/test_data.sql
@@ -0,0 +1,109 @@
+﻿delete [Route]
+go
+delete [Location]
+go
+-- Insert data for Location
+insert into [Location] values ('Tanger','TG',1)
+​
+insert into [Location] values ('Dekanariske','DK',1)
+​
+insert into [Location] values ('Tunis','TN',1)
+​
+insert into [Location] values ('Cairo','CR',1)
+​
+insert into [Location] values ('Marakesh','MG',1)
+​
+insert into [Location] values ('Tripoli','TR',1)
+​
+insert into [Location] values ('Dakar','DA',1)
+​
+insert into [Location] values ('Timbuktu','TI',1)
+​
+insert into [Location] values ('Wadai','WA',1)
+​
+insert into [Location] values ('Omdurman','OM',1)
+​
+insert into [Location] values ('Suakin','SU',1)
+​
+insert into [Location] values ('Sierra Leone','SL',1)
+​
+insert into [Location] values ('Guld Kysten','GK',1)
+​
+insert into [Location] values ('Slave Kysten','SK',1)
+​
+insert into [Location] values ('Darfur','DF',1)
+​
+insert into [Location] values ('Bahrel Ghazal','BG',1)
+​
+insert into [Location] values ('Addis Abeba','AA',1)
+​
+insert into [Location] values ('Kap Guardafui','KG',1)
+​
+insert into [Location] values ('Victoria Soen','VS',1)
+​
+insert into [Location] values ('Congo','CO',1)
+​
+insert into [Location] values ('Kabalo','KA',1)
+​
+insert into [Location] values ('Zanzibar','Za',1)
+​
+insert into [Location] values ('ST Helena','ST',1)
+​
+insert into [Location] values ('Luanda','LD',1)
+​
+insert into [Location] values ('Victoria Faldene','VF',1)
+​
+insert into [Location] values ('Mocambique','MC',1)
+​
+insert into [Location] values ('Amatave','AM',1)
+​
+insert into [Location] values ('Hvalbugten','HV',1)
+​
+insert into [Location] values ('Dragerjerget','DG',1)
+​
+insert into [Location] values ('Kap ST Marie','KM',1)
+​
+insert into [Location] values ('Kapstaden','TD',1)
+​
+-- Insert data for Route
+insert into [Route] values (15,20,1)
+​
+insert into [Route] values (15,19,1)
+​
+insert into [Route] values (19,26,1)
+​
+insert into [Route] values (19,27,1)
+​
+insert into [Route] values (37,47,1)
+​
+insert into [Route] values (20,27,1)
+​
+insert into [Route] values (27,38,1)
+​
+insert into [Route] values (27,42,1)
+​
+insert into [Route] values (42,47,1)
+​
+insert into [Route] values (20,29,1)
+​
+insert into [Route] values (29,35,1)
+​
+insert into [Route] values (35,47,1)
+​
+insert into [Route] values (18,25,1)
+​
+insert into [Route] values (25,29,1)
+​
+insert into [Route] values (25,33,1)
+​
+insert into [Route] values (32,33,1)
+​
+insert into [Route] values (33,43,1)
+​
+insert into [Route] values (47,43,1)
+​
+insert into [Route] values (47,41,1)
+​
+insert into [Route] values (47,44,1)
+​
+insert into [Route] values (32,41,1)
